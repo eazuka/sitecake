f75d70269ff8c7029904a8ebcc463be1c6a6ba12
Nikola Ivancevic <ivancevic.nikola@gmail.com>
2012-07-31 17:52:23 +0200

    Adding the global def alias DS for DIRECTORY_SEPARATOR

--- src/test/php/phpunit-bootstrap.php
+++ src/test/php/phpunit-bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+define('DS', DIRECTORY_SEPARATOR);
+
 spl_autoload_register(function($className)
 {
 	require(str_replace('_', '/', str_replace('\\', '/', ltrim($className, '\\'))) . '.php');
